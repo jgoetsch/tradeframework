be7d6aa1695173bafd1fe442de07980189a6df27
Jeremy Goetsch <jgoetsch@grandjunctioninc.com>
2020-05-01 20:04:04 -0700

    Update httpclient, spring versions

--- event-trader/src/main/java/com/jgoetsch/eventtrader/source/AbstractHttpMsgSource.java
+++ event-trader/src/main/java/com/jgoetsch/eventtrader/source/AbstractHttpMsgSource.java
@@ -15,20 +15,13 @@
  */
 package com.jgoetsch.eventtrader.source;
 
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.http.HttpException;
-import org.apache.http.HttpHost;
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpRequestInterceptor;
 import org.apache.http.NameValuePair;
 import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.AuthState;
-import org.apache.http.auth.Credentials;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.HttpClient;
@@ -37,23 +30,11 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.client.protocol.ClientContext;
 import org.apache.http.client.utils.URLEncodedUtils;
-import org.apache.http.conn.ClientConnectionManager;
-import org.apache.http.conn.scheme.PlainSocketFactory;
-import org.apache.http.conn.scheme.Scheme;
-import org.apache.http.conn.scheme.SchemeRegistry;
-import org.apache.http.conn.scheme.SocketFactory;
-import org.apache.http.impl.auth.BasicScheme;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.SingleClientConnManager;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpConnectionParamBean;
-import org.apache.http.params.HttpParams;
-import org.apache.http.params.HttpProtocolParamBean;
-import org.apache.http.protocol.ExecutionContext;
-import org.apache.http.protocol.HttpContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -62,35 +43,8 @@ public abstract class AbstractHttpMsgSource extends UrlBasedMsgSource {
 	private Logger log = LoggerFactory.getLogger(getClass());
 	private String username;
 	private String password;
-	private boolean preemtiveAuth = false;
 	private HttpClient httpClient;
 	private String method = "GET";
-	private HttpActionFilter loginAction;
-
-	static class PreemptiveAuth implements HttpRequestInterceptor {
-		public void process(
-				final HttpRequest request, 
-				final HttpContext context) throws HttpException, IOException {
-
-			AuthState authState = (AuthState) context.getAttribute(ClientContext.TARGET_AUTH_STATE);
-
-			// If no auth scheme avaialble yet, try to initialize it preemptively
-			if (authState.getAuthScheme() == null) {
-				CredentialsProvider credsProvider = (CredentialsProvider) context.getAttribute(ClientContext.CREDS_PROVIDER);
-				HttpHost targetHost = (HttpHost) context.getAttribute(ExecutionContext.HTTP_TARGET_HOST);
-
-				Credentials creds = credsProvider.getCredentials(
-						new AuthScope(
-								targetHost.getHostName(), 
-								targetHost.getPort()));
-				if (creds == null) {
-					throw new HttpException("No credentials for preemptive authentication");
-				}
-				authState.setAuthScheme(new BasicScheme());
-				authState.setCredentials(creds);
-			}
-		}
-	}
 
 	protected HttpUriRequest createRequest() {
 		List<NameValuePair> params = null;
@@ -124,33 +78,18 @@ public abstract class AbstractHttpMsgSource extends UrlBasedMsgSource {
 
 	@Override
 	protected final void receiveMsgs() {
-		/*SchemeRegistry supportedSchemes = new SchemeRegistry();
-		SocketFactory sf = PlainSocketFactory.getSocketFactory();
-		supportedSchemes.register(new Scheme("http", sf, 80));
-
-		ClientConnectionManager ccm = new SingleClientConnManager(httpParams, supportedSchemes);
-		DefaultHttpClient client = new DefaultHttpClient(httpParams);
-
-		if (getUsername() != null) {
-			client.getCredentialsProvider().setCredentials(
-				AuthScope.ANY,
-				new UsernamePasswordCredentials(getUsername(), getPassword()));
-
-			if (isPreemtiveAuth()) {
-				client.addRequestInterceptor(new PreemptiveAuth(), 0);
+		if (httpClient == null) {
+			HttpClientBuilder builder = HttpClients.custom();
+	
+			if (getUsername() != null) {
+		        CredentialsProvider credsProvider = new BasicCredentialsProvider();
+		        credsProvider.setCredentials(
+		        		AuthScope.ANY,
+		        		new UsernamePasswordCredentials(getUsername(), getPassword()));
+		        builder.setDefaultCredentialsProvider(credsProvider);
 			}
-
-			if (loginAction != null)
-				loginAction.doAction(client, this);
-		}*/
-
-		if (httpClient == null)
-			httpClient = new DefaultHttpClient();
-
-		if (getUsername() != null) {
-			((DefaultHttpClient)httpClient).getCredentialsProvider().setCredentials(
-				AuthScope.ANY,
-				new UsernamePasswordCredentials(getUsername(), getPassword()));
+			
+			httpClient = builder.build();
 		}
 
 		receiveMsgs(httpClient);
@@ -174,28 +113,12 @@ public abstract class AbstractHttpMsgSource extends UrlBasedMsgSource {
 		this.password = password;
 	}
 
-	public boolean isPreemtiveAuth() {
-		return preemtiveAuth;
-	}
-
-	public void setPreemtiveAuth(boolean preemtiveAuth) {
-		this.preemtiveAuth = preemtiveAuth;
-	}
-
 	public void setMethod(String method) {
 		this.method = method;
 	}
 
 	public String getMethod() {
 		return method;
-	}
-
-	public void setLoginAction(HttpActionFilter loginAction) {
-		this.loginAction = loginAction;
-	}
-
-	public HttpActionFilter getLoginAction() {
-		return loginAction;
 	}
 
 	public HttpClient getHttpClient() {
